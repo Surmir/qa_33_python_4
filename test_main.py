24abde8baf175fbdc931b31e59290b311f2880d6
Vasilii Kasatov <Vetquer@yandex.ru>
2025-09-29 16:36:55 +0300

    Изменен тест №9(Комментарий:Нужно исправить: тут тоже нужен тест основного сценария)

--- test_main.py
+++ test_main.py
@@ -10,12 +10,14 @@ books_genre_one = {'Denny': 'Ужасы', '67': 'Комедии', 'Faust': 'Уж
 class TestBooksCollector():
 
     #1.0 тестируем добавление книги и её повторное добавление
-    def test_add_new_book_add_book(self, collector, add_one_book):
+    def test_add_new_book_add_book(self, collector):
+        collector.add_new_book(book_one)
         collector.add_new_book(book_one)
         assert len(collector.books_genre) == 1
 
     #1.1 тестируем отсутствие жанра у добавленной книги
-    def test_add_new_book_genre_is_empty(self, collector, add_one_book):
+    def test_add_new_book_genre_is_empty(self, collector):
+        collector.add_new_book(book_one)
         assert collector.books_genre == {book_one: ''}
 
     #1.2 тестируем добавление при 0 и больше 40 символов
@@ -25,7 +27,8 @@ class TestBooksCollector():
         assert len(collector.books_genre) == 0
 
     #2.0 тестируем добавление жанра книги, если книга есть в books_genre и её жанр входит в список genre
-    def test_set_book_genre_add_genre(self, collector, add_one_book_genre):
+    def test_set_book_genre_add_genre(self, collector, book_in_dictionaries):
+        collector.set_book_genre(book_one, genre_one)
         assert collector.books_genre == {book_one: genre_one}
 
     #2.1 тестируем добавление жанра книге, при невыполнении одного или нескольких условий
@@ -33,12 +36,12 @@ class TestBooksCollector():
     @pytest.mark.parametrize('book, genre',[[book_one, 'Триллер'],
                                             ['Санта', genre_one],
                                             ['Терминатор', 'Боевик']])
-    def test_set_book_genre_genre_not_add(self, collector,add_one_book_genre, book, genre):
+    def test_set_book_genre_genre_not_add(self, collector, book_with_genre_in_dictionaries, book, genre):
         collector.set_book_genre(book, genre)
         assert collector.books_genre == {book_one: genre_one}
 
     #3.0 тестируем вывод жанра книги по её имени
-    def test_get_book_genre_get_genre(self, collector, add_one_book_genre):
+    def test_get_book_genre_get_genre(self, collector, book_with_genre_in_dictionaries):
         assert collector.get_book_genre(book_one) == genre_one
 
     #4.0 тестируем вывод списка книг с определённым жанром
@@ -47,8 +50,8 @@ class TestBooksCollector():
         assert collector.get_books_with_specific_genre(genre_one) == ['Denny', 'Faust']
     
     #5.0 тестируем вывод текущего словаря books_genre
-    def test_get_books_genre_get_dictionary_books_genre(self, collector):
-        assert collector.get_books_genre() == {}
+    def test_get_books_genre_get_dictionary_books_genre(self, collector, book_with_genre_in_dictionaries):
+        assert collector.get_books_genre() == collector.books_genre
 
     #6.0 тестируем возврат книг, которые подходят детям
     def test_get_books_for_children_get_list_books_not_in_genre_age_rating(self,collector):
@@ -56,15 +59,16 @@ class TestBooksCollector():
         assert collector.get_books_for_children() == ['67', 'Benny']
 
     #7.0 тестируем добавление книги в избранное и её повторное добавление
-    def test_add_book_in_favorites_add_book(sels, collector, add_one_book_in_favorites):
+    def test_add_book_in_favorites_add_book(sels, collector, book_in_dictionaries):
+        collector.add_book_in_favorites(book_one)
         collector.add_book_in_favorites(book_one)
         assert len(collector.favorites) == 1
 
     #8.0 тестируем удаляение книги из избранного
-    def test_delete_book_from_favorites_book_deleted(self, collector, add_one_book_in_favorites):
+    def test_delete_book_from_favorites_book_deleted(self, collector, book_in_favorites):
         collector.delete_book_from_favorites(book_one)
         assert collector.favorites == []
 
     #9.0 тестируем получение списка избранных книг
-    def test_get_list_of_favorites_books_get_list_books(self, collector):
-        assert collector.get_list_of_favorites_books() == []
+    def test_get_list_of_favorites_books_get_list_books(self, collector, book_in_favorites):
+        assert collector.get_list_of_favorites_books() == collector.favorites
